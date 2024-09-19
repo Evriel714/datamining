05e76c7eb27ee6ebdd5a63c46e7a4df5d3687f49
El <evriel.leovaztion@gmail.com>
2024-09-19 12:22:15 +0700

    Update eclat.R

--- eclat.R
+++ eclat.R
@@ -1,15 +1,16 @@
 dataset = read.csv('Groceries.csv', header = FALSE)
 dataset = dataset[-1, ]
 
-
-# install.packages('arules')
 library(arules)
 library(arulesViz)
+library(tidyverse)
+library(repr)
+library(igraph)
 # rm.duplicates is to remove duplicates because the eclat algorithm cannot have duplicates
 dataset = read.transactions('Groceries.csv', sep = ',', rm.duplicates = TRUE)
 
-rules = eclat(data = dataset, parameter = list(support = 0.003, minlen = 2 ))
+rules = eclat(data = dataset, parameter = list(support = 0.03, minlen = 2 ))
 
-inspect(sort(rules, by = 'support')[1:20])
+inspect(sort(rules, by="support")[1:10])
 
 plot(rules)
