bc55e42644ac7f0081891f63d1e4df9db1208829
El <evriel.leovaztion@gmail.com>
2024-12-05 17:39:48 +0700

    fin2

--- Preprocessing/pp_session.R
+++ Preprocessing/pp_session.R
@@ -1,14 +1,16 @@
 library(dplyr)
 library(tidyr)
 
-data <- read.csv("../Data/session_data.csv", header=TRUE)
+data_session <- read.csv("../Data/session_data.csv", header=TRUE)
 
-output <- data %>%
+anyNA(data_session)
+
+output <- data_session %>%
   group_by(session_id) %>%
   summarize(product_names = list(product_name)) %>%  # Combine product_name into a list
   unnest_wider(product_names, names_sep = "_") %>%   # Split the list into separate columns
   select(-session_id)            
 
 # Save the result to a new CSV file
-write.table(output, "../Data/dataset2.csv", sep = ",", row.names = FALSE, col.names = FALSE, na = "")
+write.table(output, "../Data/dataset_session.csv", sep = ",", row.names = FALSE, col.names = FALSE, na = "")
 
--- Preprocessing/pp_supermarket.R
+++ Preprocessing/pp_supermarket.R
@@ -1,14 +1,21 @@
 library(dplyr)
 library(tidyr)
 
-data <- read.csv("../Data/supermarket_data.csv", header=TRUE)
+data_supermarket <- read.csv("../Data/supermarket_data.csv", header=TRUE)
 
-output <- data %>%
+colSums(is.na(data_supermarket))
+anyNA(data_supermarket)
+
+data_supermarket <- data_supermarket[!is.na(data_supermarket$Customer), ]
+colSums(is.na(data_supermarket))
+anyNA(data_supermarket)
+
+output <- data_supermarket %>%
   group_by(Invoice) %>%                          # Group by memberid and date
   summarize(itemdescs = list(Description), .groups = "drop") %>% # Combine itemdesc into a list
   select(itemdescs)%>%
   unnest_wider(itemdescs, names_sep = "_") 
   
 
-write.table(output, "../Data/dataset4.csv", sep = ",", row.names = FALSE, col.names = FALSE, na = "", quote = FALSE)
+write.table(output, "../Data/dataset_supermarket.csv", sep = ",", row.names = FALSE, col.names = FALSE, na = "", quote = FALSE)
 
